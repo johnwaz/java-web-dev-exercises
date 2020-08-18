1dedcd95a833de4629014a7df631b7ba57f9f009
John Wasielewicz <wazjohn@yahoo.com>
2020-08-18 06:31:29 -0500

    Completed part of the bonus missions.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -8,19 +8,17 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius:");
 
-        while (!input.hasNextDouble()) {
-            System.out.println("Invalid");
-            input.nextLine();
+        if (!input.hasNextDouble()) {
+            System.out.println("Invalid: not a number");
+            input.close();
+        } else {
+            double radius = input.nextDouble();
+            if (radius < 0) {
+                System.out.println("Invalid: negative number");
+            } else {
+                double area = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius " + radius + " is " + area);
+            }
         }
-
-        double radius = input.nextDouble();
-        while (radius < 0) {
-            System.out.println("Invalid negative number");
-            input.nextLine();
-        }
-
-        double area = 3.14 * radius * radius;
-        System.out.println("The area of a circle of " + radius + " is " + area);
-
     }
 }
