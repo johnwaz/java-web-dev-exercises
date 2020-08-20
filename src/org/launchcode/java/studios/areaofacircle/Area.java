4497454588d245b2838b435357cde16f778690f8
John Wasielewicz <wazjohn@yahoo.com>
2020-08-19 21:15:25 -0500

    Made a few minor adjustments.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -6,19 +6,25 @@ public class Area {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a radius:");
+        System.out.println("Enter a radius: ");
 
-        if (!input.hasNextDouble()) {
-            System.out.println("Invalid: not a number");
-            input.close();
-        } else {
-            double radius = input.nextDouble();
-            if (radius < 0) {
-                System.out.println("Invalid: negative number");
-            } else {
-                double area = Circle.getArea(radius);
-                System.out.println("The area of a circle of radius " + radius + " is " + area);
-            }
+        String radiusInput = input.nextLine();
+        while (!isValidInput(radiusInput)) {
+            System.out.println("Invalid: negative number");
+            radiusInput = input.nextLine();
         }
+
+        double radius = Double.parseDouble(radiusInput);
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is " + area);
+
+
+    }
+
+    private static boolean isValidInput(String input) {
+        if(Double.parseDouble(input) < 0) {
+            return false;
+        }
+        return true;
     }
 }
